fcc242b0a7057f50ab3b36dd339b3317d648182f
yoohooitstoo <stumanscott@gmail.com>
2020-06-11 11:06:56 -0400

    added a for loop to run through the array

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,56 +1,35 @@
 // DEFINE VARIABLES
-var myQuiz = [
-    {
-      question: "Commonly used data types DO NOT include",
-      answers: {
-        1: "strings",
-        2: "booleans",
-        3: "alerts",
-        4: "alerts"
+var myQuiz = [{
+    questionString: "Commonly used data types DO NOT include",
+    answersArray: ["strings", "booleans", "alerts", "alerts"],
+    correctAnswer: 3
+  },
+  {
+    questionString: "The condition in an if / else statement is enclosed within________",
+    answersArray: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    correctAnswer: 2
+  },
+  {
+    questionString: "Arrays in JavaScript can be used to store_____",
+    answersArray: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    correctAnswer: 4
+  },
+  {
+    questionString: "String values must be enclosed within _______ when being assigned to variables",
+    answersArray: ["commas", "curly brackets", "quotes", "parentheses"],
+    correctAnswer: 3
+  },
+  {
+    questionString: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+    answersArray: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    correctAnswer: 4
+  },
+]; console.log(myQuiz);
 
-      },
-      correctAnswer: 3
-    },
-    {
-      question: "The condition in an if / else statement is enclosed within________",
-      answers: {
-        1: "quotes",
-        2: "curly brackets",
-        3: "parentheses",
-        4: "square brackets"
-      },
-      correctAnswer: 2
-    },
-    {
-      question: "Arrays in JavaScript can be used to store_____",
-      answers: {
-        1: "numbers and strings",
-        2: "other arrays",
-        3: "booleans",
-        4: "all of the above"
-      },
-      correctAnswer: 4
-    },
-    {
-      question: "String values must be enclosed within _______ when being assigned to variables",
-      answers: {
-        1: "commas",
-        2: "curly brackets",
-        3: "quotes",
-        4: "parentheses"
-    },
-      correctAnswer: 3
-    },
-    {
-      question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
-      answers: {
-        1: "JavaScript",
-        2: "terminal/bash",
-        3: "for loops",
-        4: "console.log"
-      },
-      correctAnswer: 4
-    },
-  ];
-
-  console.log(myQuiz);
+for (var i = 0; i, myQuiz.length; i++) {
+  if (i === myQuiz[0].correctAnswer) {
+    console.log("that answer is correct");
+  } else {
+    console.log("that answer is wrong");
+  }
+};
