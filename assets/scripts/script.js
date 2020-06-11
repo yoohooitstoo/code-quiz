56fe11a9f4714f804a7152b79ff34669e3b5a851
yoohooitstoo <stumanscott@gmail.com>
2020-06-11 18:00:01 -0400

    added a function to display quiz questions

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,35 +1,56 @@
 // DEFINE VARIABLES
-var myQuiz = [{
-    questionString: "Commonly used data types DO NOT include",
-    answersArray: ["strings", "booleans", "alerts", "alerts"],
-    correctAnswer: 3
+var 
+var myQuizArray = [{
+    questionStringOne: "Commonly used data types DO NOT include",
+    answersArrayOne: ["strings", "booleans", "alerts", "alerts"],
+    correctAnswerOne: 3
   },
   {
-    questionString: "The condition in an if / else statement is enclosed within________",
-    answersArray: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    correctAnswer: 2
+    questionStringTwo: "The condition in an if / else statement is enclosed within________",
+    answersArrayTwo: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    correctAnswerTwo: 2
   },
   {
-    questionString: "Arrays in JavaScript can be used to store_____",
-    answersArray: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-    correctAnswer: 4
+    questionStringThree: "Arrays in JavaScript can be used to store_____",
+    answersArrayThree: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    correctAnswerThree: 4
   },
   {
-    questionString: "String values must be enclosed within _______ when being assigned to variables",
-    answersArray: ["commas", "curly brackets", "quotes", "parentheses"],
-    correctAnswer: 3
+    questionStringFour: "String values must be enclosed within _______ when being assigned to variables",
+    answersArrayFour: ["commas", "curly brackets", "quotes", "parentheses"],
+    correctAnswerFour: 3
   },
   {
-    questionString: "A very useful tool used during development and debugging for printing content to the debugger is: ",
-    answersArray: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-    correctAnswer: 4
+    questionStringFive: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+    answersArrayFive: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    correctAnswerFive: 4
   },
-]; console.log(myQuiz);
+]; console.log(myQuizArray);
 
-for (var i = 0; i, myQuiz.length; i++) {
-  if (i === myQuiz[0].correctAnswer) {
-    console.log("that answer is correct");
-  } else {
-    console.log("that answer is wrong");
-  }
-};
+// DEFINE FUNCTIONS
+function setmyQuiz(){
+  // Use DOM Manipulation to take an array and put it on the page
+  var myQuiz = JSON.parse(myQuizArray);
+  document.getElementById("my-quiz").innerHTML = myQuiz.questionStringOne;
+}
+  
+
+// FUNCTION CALLS
+
+
+
+// EVENT LISTENERS
+
+
+
+
+
+
+
+// for (var i = 0; i, myQuiz.length; i++) {
+//   if (i === myQuiz[0].correctAnswer) {
+//     console.log("that answer is correct");
+//   } else {
+//     console.log("that answer is wrong");
+//   }
+// };
