ca3cd3dec39891c556bacfa482ec4016ae780bfa
yoohooitstoo <stumanscott@gmail.com>
2020-06-11 18:32:41 -0400

    Added button and cycled through questions

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,46 +1,56 @@
 // DEFINE VARIABLES
-var 
 var myQuizArray = [{
-    questionStringOne: "Commonly used data types DO NOT include",
-    answersArrayOne: ["strings", "booleans", "alerts", "alerts"],
-    correctAnswerOne: 3
+    question: "Commonly used data types DO NOT include",
+    answers: ["strings", "booleans", "numbers", "alerts"],
+    correctAnswer: 3
   },
   {
-    questionStringTwo: "The condition in an if / else statement is enclosed within________",
-    answersArrayTwo: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    correctAnswerTwo: 2
+    question: "The condition in an if / else statement is enclosed within________",
+    answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    correct: 2
   },
   {
-    questionStringThree: "Arrays in JavaScript can be used to store_____",
-    answersArrayThree: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-    correctAnswerThree: 4
+    question: "Arrays in JavaScript can be used to store_____",
+    answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    correct: 4
   },
   {
-    questionStringFour: "String values must be enclosed within _______ when being assigned to variables",
-    answersArrayFour: ["commas", "curly brackets", "quotes", "parentheses"],
-    correctAnswerFour: 3
+    question: "String values must be enclosed within _______ when being assigned to variables",
+    answers: ["commas", "curly brackets", "quotes", "parentheses"],
+    correct: 3
   },
   {
-    questionStringFive: "A very useful tool used during development and debugging for printing content to the debugger is: ",
-    answersArrayFive: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-    correctAnswerFive: 4
+    question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+    answers: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    correct: 4
   },
-]; console.log(myQuizArray);
+];
+console.log(myQuizArray);
+
+
+
+
 
 // DEFINE FUNCTIONS
-function setmyQuiz(){
+function setmyQuiz() {
   // Use DOM Manipulation to take an array and put it on the page
-  var myQuiz = JSON.parse(myQuizArray);
-  document.getElementById("my-quiz").innerHTML = myQuiz.questionStringOne;
+  document.getElementById("my-quiz").innerHTML = myQuizArray[0].question;
 }
-  
+
+setmyQuiz();
 
 // FUNCTION CALLS
+questionNumber = 0;
+function nextQuestion()
+{
+  document.getElementById("my-quiz").innerHTML = myQuizArray[questionNumber].question;
+  questionNumber++;
 
+}
 
 
 // EVENT LISTENERS
-
+document.getElementById("next").addEventListener("click", nextQuestion)
 
 
 
@@ -54,3 +64,26 @@ function setmyQuiz(){
 //     console.log("that answer is wrong");
 //   }
 // };
+
+
+
+
+// As you proceed in your career as a web developer, 
+// you will probably be asked to complete a coding assessment, 
+// which is typically a combination of multiple - choice questions 
+// and interactive challenges.Build a timed code quiz with multiple - 
+// choice questions.This app will run in the browser and feature 
+// dynamically updated HTML and CSS powered by your JavaScript code.
+// It will also feature a clean and polished user interface and be 
+// responsive, ensuring that it adapts to multiple screen sizes.
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
