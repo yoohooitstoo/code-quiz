fc0f5b69d12639dd9d31d1dfe88a04f808e33bee
yoohooitstoo <stumanscott@gmail.com>
2020-06-10 22:15:33 -0400

    added a inde and script wrote out my array

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # code-quiz
+### Create Initial Files
+
+1. Create index.html - test with "Hello World"
+2. Create script.js - test by console.log("Hello world");
+3. Import Bootstrap into the HTML file Head tag.
+
+
+### Pseudocode
+
+1. Based on the wireframe, build out the basic structure of the page using Bootstrap classes.
+2. Structure out your script.js file.
+
+#### OPTION 1 - hard code everything
+
+#### OPTION 2 - Build our data
+
+
+
+
+## Troubleshooting
+
+1. Compare non-working code with working code from a different project or activity.
+2. Console.log everything, something? 
---
+++ assets/scripts/script.js
@@ -0,0 +1,54 @@
+// DEFINE VARIABLES
+var myQuiz = [
+    {
+      question: "Commonly used data types DO NOT include",
+      answers: {
+        1: "strings",
+        2: "booleans",
+        3: "alerts",
+        4: "alerts"
+
+      },
+      correctAnswer: 3
+    },
+    {
+      question: "The condition in an if / else statement is enclosed within________",
+      answers: {
+        1: "quotes",
+        2: "curly brackets",
+        3: "parentheses",
+        4: "square brackets"
+      },
+      correctAnswer: 2
+    },
+    {
+      question: "Arrays in JavaScript can be used to store_____",
+      answers: {
+        1: "numbers and strings",
+        2: "other arrays",
+        3: "booleans",
+        4: "all of the above"
+      },
+      correctAnswer: 4
+    },
+    {
+      question: "String values must be enclosed within _______ when being assigned to variables",
+      answers: {
+        1: "commas",
+        2: "curly brackets",
+        3: "quotes",
+        4: "parentheses"
+    },
+      correctAnswer: 3
+    },
+    {
+      question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+      answers: {
+        1: "JavaScript",
+        2: "terminal/bash",
+        3: "for loops",
+        4: "console.log"
+      },
+      correctAnswer: 4
+    },
+  ];
